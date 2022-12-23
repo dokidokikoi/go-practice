b2eb4a4eb0bf5152aef4984eeb601c28b8c2a16d
daiyanqiang <yanqiang.dai@wudun.net>
2022-12-23 16:00:47 +0800

    grpc 使用 etcd 服务发现

--- grpc/helloworld/greeter_client/main.go
+++ grpc/helloworld/greeter_client/main.go
@@ -8,6 +8,8 @@ import (
 
 	pb "helloworld/helloworld"
 
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/naming/resolver"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -19,10 +21,30 @@ var (
 	name = flag.String("name", defaultName, "Name to greet")
 )
 
+var cli *clientv3.Client
+
+func init() {
+	var err error
+	cli, err = clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: time.Second * 10,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	flag.Parse()
 
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// resp, _ := cli.Get(context.Background(), "foo/bar/my-service")
+	// conn, err := grpc.Dial(string(resp.Kvs[0].Value), grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	etcdResolver, err := resolver.NewBuilder(cli)
+	conn, err := grpc.Dial(
+		"etcd:///foo/bar/my-service",
+		grpc.WithResolvers(etcdResolver),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect; %v", err)
 	}
--- grpc/helloworld/greeter_server/main.go
+++ grpc/helloworld/greeter_server/main.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	pb "helloworld/helloworld"
 
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/naming/endpoints"
 	"google.golang.org/grpc"
 )
 
@@ -23,6 +26,19 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelleRe
 	return &pb.HelleReply{Message: "Hello " + in.GetName()}, nil
 }
 
+var cli *clientv3.Client
+
+func init() {
+	var err error
+	cli, err = clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: time.Second * 10,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
@@ -32,6 +48,32 @@ func main() {
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
+
+	resp, err := cli.Grant(context.TODO(), 5)
+	if err != nil {
+		panic(err)
+	}
+	// if _, err = cli.Put(
+	// 	context.TODO(), "foo/bar/my-service",
+	// 	fmt.Sprintf("localhost:%d", *port),
+	// 	clientv3.WithLease(resp.ID)); err != nil {
+	// 	panic(err)
+	// }
+
+	em, _ := endpoints.NewManager(cli, "foo/bar/my-service")
+	err = em.AddEndpoint(
+		context.TODO(),
+		"foo/bar/my-service/v1",
+		endpoints.Endpoint{Addr: fmt.Sprintf("localhost:%d", *port)},
+		clientv3.WithLease(resp.ID))
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err = cli.KeepAlive(context.TODO(), resp.ID); err != nil {
+		panic(err)
+	}
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
